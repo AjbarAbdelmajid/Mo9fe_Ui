16f62d5206561c2f89ff1cbe70d3cea173afb971
mjid <ajbareabdlmjid@gmail.com>
2019-05-30 20:05:20 +0000

    [Update] update footer

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 //import { ModalFooter } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-const NavBar = () => {
-    return(
-        <div className="card-footer Footer">
-            <ul>
-                <li>tep1</li>
-                <li>tep1</li>
-            </ul>
+
+const Footer = ()=> {
+    return (
+        <div>
+            <div className="theSpace" />
+            <div className="theFooter">
+
+            </div>
         </div>
     )
 };
 
-export default NavBar
+export default Footer
